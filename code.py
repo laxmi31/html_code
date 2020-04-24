672e0a1f0cc14add0c8660260321211a47627fef
Pratap Singh <AG65109@ad.wellpoint.com>
2020-04-24 17:21:34 +0530

    first Commit

---
+++ code.py
@@ -0,0 +1,52 @@
+<html>
+ <head>
+   <title> Login page </title>
+  </head>
+  <body style="background-color:cyan">
+  
+  <p>Hello World</p> 
+  <p>This is my first login page!<p/>
+  <a href="http://www.google.com">google.com</a>
+  <h1>SIGN UP</h1>
+  <h2>Please fill the form below</h2>
+  <!--s
+  <img src="https://www.google.com/search?q=flower&rlz=1C1GCEB_enIN883IN883&source=lnms&tbm=isch&sa=X&ved=2ahUKEwir3Oe1sfnoAhXEGc0KHcBeDK8Q_AUoAXoECBMQAw&biw=1536&bih=754&dpr=1.25&safe=active&ssui=on#imgrc=LS8O0DgJ_PfmAM" >
+  <img src="C:\Users\ag65109\Desktop\LAXMI_DOCS\Himmu_Photo.jpg" height="400" width="300">
+  -->
+  <label> First Name </label> 
+  <input type="text"> <br><br>
+  <label>Last name</label>
+  <input type="text"><br><br>
+  <label>email</label>
+  <input type="email"><br><br>
+  <label>password</label>
+  <input type="password"><br><br>
+  <button type="button">login </button>
+  <!-- this code is for link through an image -->
+  <a href="http://google.com"><img src="C:\Users\ag65109\Desktop\LAXMI_DOCS\Himmu_Photo.jpg"height="400" width="300">
+  <a href="mailto:mamtamehra07@gmail.com">ME BRO</a>
+  <ul>
+  <li>LAXMI</li>
+  <li>HIMMU</li>
+  <li>MOMMY</li>
+  <li>MAMTA</li>
+  </ul>
+  <ol>
+  <li>LAXMI</li>
+  <li>HIMMU</li>
+  <li>MOMMY</li>
+  <li>MAMTA</li>
+  </ul>
+  
+  <table border style="color:yellow">
+  <tr>
+  <th>NAME</th>
+  <th>COURSE</th>
+  <th>MARKS</th>
+  </tr>
+  <td>CHADI</td>
+  <td>SUSU</td>
+  <td>0</td>
+  </table>
+  </body>
+</html>
